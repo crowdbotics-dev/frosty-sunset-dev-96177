85cb1e747240bda6bff29ebab6245958f12722a1
Crowdbotics <engineering@crowdbotics.com>
2023-06-30 02:15:11 +0000

    Initial commit.

---
+++ backend/frosty_sunset_dev_96177/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for frosty_sunset_dev_96177 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frosty_sunset_dev_96177.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-sunset-dev-96177",
+  url: "https://frosty-sunset-dev-96177.botics.co",
+  api: "https://frosty-sunset-dev-96177.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
